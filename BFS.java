bbcd878ed984f815c73a5f0e0ba9f4d8d5de11e8
mayank raghuvanshi <mayank1272@hotmail.com>
2020-01-14 12:53:12 +0530

    BFS

--- BFS.java
+++ BFS.java
@@ -2,31 +2,23 @@ import java.util.*;
 import utils.Graph;
 import utils.Edge;
 class main{
-    //using recursion.
-    static void BFS(ArrayList<Edge> graph[] , int src , boolean isVisited[]){
-        System.out.print(src+"->");
-        isVisited[src]=true;
-        for(Edge e : graph[src]){
-            if(!isVisited[e.vertex]){
-                BFS(graph, e.vertex , isVisited);
-            }
-        }
-    }
+
     //using stack
     static void BFS(ArrayList<Edge> graph[] , int src){
         boolean isVisited[] = new boolean[graph.length];
-        Stack<Integer> stack = new Stack<>();
-        stack.push(src);
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(src);
         StringBuilder output = new StringBuilder();
         isVisited[src]=true;
-        while(stack.size()>0){
-            int popedVertex = stack.pop();
-            output.append(popedVertex+"->");
-            for(Edge e : graph[popedVertex]){
+        while(queue.size()>0){
+            int enquedVertex = queue.peek();
+            queue.remove();
+            output.append(enquedVertex+"->");
+            for(Edge e : graph[enquedVertex]){
                 int currentVertex = e.vertex;
                 if(!isVisited[currentVertex]){
                     isVisited[currentVertex]=true;
-                    stack.push(currentVertex);
+                    queue.add(currentVertex);
                 }
             }
         }
@@ -43,8 +35,6 @@ class main{
         g.addEdge(5,6,16,true);
         g.addEdge(5,7,17,true);
         g.addEdge(6,7,19,true);
-        BFS(graph,1,new boolean[graph.length]);
-        System.out.println();
         BFS(graph,1);
                
     }
