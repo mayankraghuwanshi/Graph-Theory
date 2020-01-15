224e515631d24ddec76e5eaa51e4d23be5fbd81f
mayank raghuvanshi <mayank1272@hotmail.com>
2020-01-15 16:24:43 +0530

    BFS

--- BFS.java
+++ BFS.java
@@ -4,13 +4,13 @@ import utils.Edge;
 class main{
     //NOTE: BFS can't detect cycle in a directed graph.
     //Below implementation is can only work with undirected graph.
-    static void BFS(int src , int des, ArrayList<Edge> graph[] ){
-        boolean isVisited [] = new boolean[graph.length];
+    static void BFS(int src , int des , ArrayList<Edge> graph[]){
+        boolean isVisited[] = new boolean[graph.length];
         Queue<Integer> queue = new LinkedList<>();
-        int cycle=0;
-        int level=0;
+        int level = 0;
         queue.add(src);
         queue.add(-1);
+        isVisited[src]=true;
         while(queue.size()>1){
             int currentVertex = queue.remove();
             if(currentVertex==-1){
@@ -18,59 +18,93 @@ class main{
                 level++;
                 continue;
             }
-            if(isVisited[currentVertex]){
-                cycle++;
-                System.out.println("Cycle detected @ "+currentVertex);
-                continue;
-            }
             if(currentVertex==des){
-                System.out.println("Vertex "+des+" has found at level "+level+ " from "+src);
+                    System.out.println("Vertex "+des+" has found at level "+level+" from source "+src);
             }
-            isVisited[currentVertex]=true;
             for(Edge e : graph[currentVertex]){
-                queue.add(e.vertex);
+                int neighbour = e.vertex;
+                if(!isVisited[neighbour]){
+                    queue.add(neighbour);
+                }
             }
         }
     }
+   
+
     static void BFS2(int src , int des , ArrayList<Edge> graph[]){
         boolean isVisited[] = new boolean[graph.length];
         Queue<Integer> queue = new LinkedList<>();
         queue.add(src);
         int level=0;
-        int cycle=0;
+        isVisited[src]=true;
         while(queue.size()>0){
             int size = queue.size();
             while(size-->0){
                 int currentVertex = queue.remove();
-                if(isVisited[currentVertex]){
-                    cycle++;
-                    System.out.println("Cycle detected @ "+currentVertex);
-                    continue;
-                }
-                isVisited[currentVertex]=true;
                 if(currentVertex==des){
-                    System.out.println("Vertex "+des+" has found at level "+level+ " from "+src);
+                    System.out.println("Vertex "+des+" has found at level "+level+" from source "+src);
                 }
                 for(Edge e : graph[currentVertex]){
-                    queue.add(e.vertex);
+                    int neighbour = e.vertex;
+                    if(!isVisited[neighbour]){
+                        queue.add(neighbour);
+                    }
                 }
             }
             level++;
         }
     }
+
+    static void BFS3(int src , int des , ArrayList<Edge>graph[]){
+        int n = graph.length;
+        int parent[] = new int[n];
+        int distance[] = new int[n];
+        boolean isVisited[] = new boolean[n];
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(src);
+        isVisited[src]=true;
+        distance[src]=0;
+        parent[src]=-1;
+        while(queue.size()>0){
+            int currentVertex = queue.remove();
+            if(currentVertex==des){
+                System.out.println("Vertex "+des+" has found at level "+distance[currentVertex]+" from source "+src);
+                int tempVertex = currentVertex;
+                int tempParent = parent[currentVertex];
+                while(tempParent!=-1){
+                    System.out.print(tempVertex+"<-");
+                    tempVertex=tempParent;
+                    tempParent=parent[tempParent];
+                }
+                System.out.println(tempVertex);               
+            }
+            for(Edge e : graph[currentVertex]){
+                int neighbour = e.vertex;
+                if(!isVisited[neighbour]){
+                    isVisited[neighbour]=true;
+                    queue.add(neighbour);
+                    parent[neighbour]=currentVertex;
+                    distance[neighbour]=distance[currentVertex]+1;
+                }
+            }
+        }
+    }
+
     public static void main(String args[]){
-        Graph g = new Graph(5);
+        Graph g = new Graph(6);
         ArrayList<Edge> graph[] = g.graph;
-        g.addEdge(0,3,2,false);
+        g.addEdge(0,4,2,false);
         g.addEdge(0,1,10,false);
         g.addEdge(1,2,13,false);
-        g.addEdge(3,4,12,false);
-        g.addEdge(4,2,15,false);
-        // g.addEdge(5,6,16,true);
-        // g.addEdge(5,7,17,true);
-        // g.addEdge(6,7,19,true);
-        // BFS(1,6,graph);
-        BFS2(0,4,graph);
+        g.addEdge(2,4,12,false);
+        g.addEdge(2,3,15,false);
+        g.addEdge(3,5,15,false);
+        g.addEdge(4,3,15,false);
+        // g.display();
+        // BFS(0,4,graph);
+        // BFS2(0,4,graph);
+        BFS3(0,3,graph);
+
                
     }
 }
---
+++ CC.java
@@ -0,0 +1,36 @@
+import utils.*;
+import java.util.*;
+//connected component in graph
+class main{
+    public static void main(String args[]){
+        Graph g = new Graph(7);
+        ArrayList<Edge> graph[] = g.graph;
+        g.addEdge(1,3,2,true);
+        g.addEdge(1,2,10,true);
+        g.addEdge(2,4,13,true);
+        g.addEdge(3,4,12,true);
+        g.addEdge(3,5,15,true);
+        g.addEdge(5,6,16,true);
+        g.addEdge(5,7,17,true);
+        g.addEdge(6,7,19,true);
+        g.display();
+        //using bfs.
+        Queue<Integer> queue = new LinkedList<>();
+        boolean isVisited[] = new boolean[graph.length];
+        int count=0;
+        for(int i=0;i<graph.length;i++){
+            if(!isVisited[i]){
+                queue.add(i);
+                count++;
+            }
+            while(queue.size()>0){
+                int currentVertex = queue.remove();
+                isVisited[currentVertex]=true;
+                for(Edge e : graph[currentVertex]){
+                    if(!isVisited[e.vertex]) queue.add(e.vertex);
+                }
+            }
+        }
+        System.out.println(count);
+    }
+}
